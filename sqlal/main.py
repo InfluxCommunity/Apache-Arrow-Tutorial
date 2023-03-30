d001da338e9d88e38052cfac49bc10809d203afb
Jay Clifford <jclifford@influxdata.com>
2023-03-30 15:27:37 +0100

    added sqlalchemy example

---
+++ sqlal/main.py
@@ -0,0 +1,20 @@
+import flightsql.sqlalchemy
+from sqlalchemy import func, select
+from sqlalchemy.engine import create_engine
+from sqlalchemy.schema import MetaData, Table
+
+token = "6mFPNdcEwrjQD9utxkkMS6BfmhJoMIYsHkI317EcGSCMZaTalYADf0zm6u4VqrBv5YiGvvOf5Qa8sYTVrDigeA=="
+host = "eu-central-1-1.aws.cloud2.influxdata.com"
+bucket = "mlops"
+
+engine = create_engine(f"datafusion+flightsql://{host}:443?bucket-name={bucket}&token={token}")
+
+runs = Table("BW_Inference_results", MetaData(bind=engine), autoload=True)
+count = select([func.count("*")], from_obj=runs).scalar()
+print("runs count:", count)
+print("columns:", [(r.name, r.type) for r in runs.columns])
+
+# Reflection
+metadata = MetaData(schema="iox")
+metadata.reflect(bind=engine)
+print("tables:", [table for table in metadata.sorted_tables])
